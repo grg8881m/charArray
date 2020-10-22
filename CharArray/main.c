38016d4d4ef6b4de81a2334df51e067ea3f648b3
Giovanni Rodriguez G <34606945+grg8881m@users.noreply.github.com>
2020-10-22 05:48:41 -0500

    Add files via upload

---
+++ CharArray/main.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    printf("Cadena de Caracteres\n");
+
+    char nameC[50];
+
+    int size;
+
+    printf("Ingresar el nombre con gets: \n");
+
+    gets(nameC);
+
+    printf("El nombre es: ");
+
+    puts(nameC);
+
+    size = strlen(nameC);
+
+    printf("\n El tamanno de la cadena es: %i \n", size);
+
+    return 0;
+}
